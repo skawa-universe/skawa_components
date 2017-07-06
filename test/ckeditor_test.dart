022f7c2e125566a435687cee168061f8a8f5502f
Daniel Varga <dvarga@skawa.hu>
2017-07-06 14:02:19 +0200

    (fix) fixed invalid import

--- test/ckeditor_test.dart
+++ test/ckeditor_test.dart
@@ -1,8 +1,8 @@
 @Tags(const ['aot'])
 @TestOn('browser')
 import 'package:angular2/angular2.dart';
-import 'package:angular_ckeditor/ckeditor.dart';
 import 'package:angular_test/angular_test.dart';
+import 'package:skawa_components/src/components/ckeditor/ckeditor.dart';
 import 'package:test/test.dart';
 
 @AngularEntrypoint()
