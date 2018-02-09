446aac381191c362d9654697813a6a0b83dda9ea
Harsányi Tamás <tharsanyi@skawa.hu>
2018-02-09 13:41:57 +0100

    (travis) added more test run to travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -6,7 +6,10 @@ set -ev
 #dartfmt lib/ --line-length=120 --set-exit-if-changed -n
 # dartfmt test/ --line-length=120 --set-exit-if-changed -n
 
+xvfb-run -s "-screen 0 1024x768x24" pub run angular_test --test-arg=-pdartium --test-arg=test/editor_render_source_test.dart
+xvfb-run -s "-screen 0 1024x768x24" pub run angular_test --test-arg=-pdartium --test-arg=test/editor_render_source_test.dart
 xvfb-run -s "-screen 0 1024x768x24" pub run angular_test --test-arg=-pdartium --test-arg=test/stabilizator_test.dart
+xvfb-run -s "-screen 0 1024x768x24" pub run angular_test --test-arg=-pdartium --test-arg=test/editor_render_source_test.dart
 # xvfb-run -s "-screen 0 1024x768x24" pub run test test/base_grid_test.dart -pdartium
 
 # dart tool/grind.dart
