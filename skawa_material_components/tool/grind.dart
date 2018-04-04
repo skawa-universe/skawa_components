3044e30f62e111274551c464c6ef81a82996793d
Harsányi Tamás <tharsanyi@skawa.hu>
2018-04-04 15:00:48 +0200

    (travis) logging added to test running

--- skawa_material_components/tool/grind.dart
+++ skawa_material_components/tool/grind.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:angular_utility/src/grind.dart' as ang;
 import 'package:angular_utility/src/process_start_task.dart' as ang;
 import 'package:grinder/grinder.dart';
+import 'package:ansicolor/ansicolor.dart';
+import 'package:logging/logging.dart';
 
 Future<Null> main(List<String> args) async => grind(args);
 
@@ -27,6 +30,8 @@ Future sasswatch() async {
 @Task('run test')
 Future test(GrinderContext args) async {
   bool dry = args.invocation.arguments.getFlag('dry');
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) => logEvent(rec, !dry));
   ang.ServeProcess serveProcess = new ang.ServeProcess();
   await serveProcess.processStart(pubServe, dry);
   await ang.processStart(normalTest, dry);
@@ -52,3 +57,36 @@ const ang.ProcessInformation angularTest = const ang.ProcessInformation(
       '--test-arg=--pub-serve=8080'
     ],
     'Run angular_test');
+
+/// Prints a log event to stdout
+void logEvent(LogRecord rec, bool verbose) {
+  var color;
+  if (rec.level == Level.WARNING) {
+    color = _yellow;
+  } else if (rec.level == Level.SEVERE) {
+    color = _red;
+  } else if (rec.level == Level.FINER) {
+    color = _blue;
+  } else if (rec.level == Level.INFO) {
+    color = _cyan;
+  } else if (rec.level == Level.FINEST) {
+    if (!verbose) return;
+    color = _magenta;
+  } else {
+    color = _green;
+  }
+  stdout.writeln(color('[${rec.level.name}]\t${rec.time.toLocal()}\t${rec.message}'));
+  if (rec.error != null) {
+    stdout.writeln(color('\n${rec.error}'));
+    if (rec.stackTrace != null) {
+      stdout.writeln(color('${rec.stackTrace}'));
+    }
+  }
+}
+
+final _green = new AnsiPen()..green();
+final _red = new AnsiPen()..red();
+final _yellow = new AnsiPen()..yellow();
+final _blue = new AnsiPen()..blue(bold: true);
+final _cyan = new AnsiPen()..cyan();
+final _magenta = new AnsiPen()..magenta(bold: true);
