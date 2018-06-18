b89a64b842c9f101116d397da8423c03ada16de1
Harsányi Tamás <tharsanyi@skawa.hu>
2018-06-18 15:25:45 +0200

    (feat) SkawaMarkdownRenderComponent added

---
+++ lib/markdown_renderer/markdown_renderer.dart
@@ -0,0 +1,29 @@
+import 'package:angular/angular.dart';
+import 'package:skawa_components/directives/language_direction_directive.dart';
+import 'package:skawa_components/markdown_editor/editor_render_target.dart';
+
+@Component(
+    selector: 'skawa-markdown-renderer',
+    templateUrl: 'markdown_renderer.html',
+    styleUrls: const ['markdown_renderer.css'],
+    directives: const [EditorRenderTarget, LanguageDirectionDirective],
+    changeDetection: ChangeDetectionStrategy.OnPush)
+class SkawaMarkdownRendererComponent implements OnInit {
+  String _source;
+  String _emulatedCssClass;
+
+  @ViewChild(EditorRenderTarget)
+  EditorRenderTarget editorRenderTarget;
+
+  @Input()
+  set source(String source) {
+    _source = source;
+    editorRenderTarget.updateRender(_source, classes: [_emulatedCssClass]);
+  }
+
+  @override
+  void ngOnInit() {
+    _emulatedCssClass = editorRenderTarget.elementRef.nativeElement.classes
+        .firstWhere((String cssClass) => !cssClass.contains('markdown-container'));
+  }
+}
